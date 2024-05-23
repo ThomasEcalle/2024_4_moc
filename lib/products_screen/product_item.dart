7abc66eddeecc6216c83c132562b027ad3d77fe0
ThomasEcalle <thomasecalle@gmail.com>
2024-05-23 12:08:06 +0200

    Add buildwhen on products screen

--- lib/products_screen/product_item.dart
+++ lib/products_screen/product_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moc_4_2924/models/product.dart';
+import 'package:moc_4_2924/products_screen/cart_bloc/cart_bloc.dart';
 
 class ProductItem extends StatelessWidget {
   const ProductItem({
@@ -16,6 +18,20 @@ class ProductItem extends StatelessWidget {
     return ListTile(
       title: Text(product.name),
       subtitle: Text(product.price.toString()),
+      trailing: BlocBuilder<CartBloc, CartState>(
+        buildWhen: (previousState, nextState) {
+          if (nextState.status != CartStatus.addedProductWithSuccess) return false;
+          final previousOccurrences = previousState.products.where((element) => element.id == product.id).length;
+          final nextOccurrences = nextState.products.where((element) => element.id == product.id).length;
+          return nextOccurrences != previousOccurrences;
+        },
+        builder: (context, state) {
+          print('Building ${product.id} product');
+          final occurrences = state.products.where((element) => element.id == product.id).length;
+          if (occurrences == 0) return const SizedBox();
+          return Text('$occurrences dans panier');
+        },
+      ),
       onTap: onTap,
     );
   }
